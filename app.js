d041510fb2818f8564fb0b8e6e087029b2dba40a
Alex-Lar <doremilarin@gmail.com>
2021-12-24 20:48:51 +0300

    first com

---
+++ app.js
@@ -0,0 +1,32 @@
+const toDoList = [];
+let userQuery;
+let userTodos;
+
+while (userQuery !== "quit") {
+    userQuery = prompt("What would you like to do?", "");
+
+    if (userQuery === "new") {
+        userTodos = prompt("Enter new todo:", "");
+        toDoList.push(userTodos);
+    } else if (userQuery === "delete") {
+        userTodos = parseInt(prompt("Enter the task number:", ""));
+        toDoList.splice(userTodos, 1);
+    } else if (userQuery === "list") {
+        console.log("***************");
+        for(let i = 0; i < toDoList.length; i++) {
+            console.log(`${i}: "${toDoList[i]}"`);
+        }
+        console.log("***************");
+    }
+}
+
+console.log("You quit app. Thanks for using. Goodbye!");
+
+
+
+
+// "new" - Add A Todo
+// "list" - List All Todos
+// "delete" - Remove Specific Todo
+// "quit" - Quit App
+
